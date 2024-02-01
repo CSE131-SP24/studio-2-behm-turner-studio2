506c3123be19767c2979d27c5b3c881256950c29
ingrid <ingrid@localhost>
2024-02-01 11:19:34 -0600

    commit Studio 2

--- src/studio2/Pi.java
+++ src/studio2/Pi.java
@@ -10,8 +10,16 @@ public class Pi {
 	double dartThrowY = Math.random ();
 	System.out.println("Dart coordinates: " + dartThrowX + "," + dartThrowY);
 	//Origin is 0,0 ; r=1
-	double distance = Math.sqrt((dartThrowX)*(dartThrowX)+ (dartThrowY)*(dartThrowY));
+	double distance = Math.sqrt((dartThrowX)*(dartThrowX)*100/100.00+ (dartThrowY)*(dartThrowY));
 	System.out.println ("Dart distance: " + distance);
-	
+int count = 0;
+while (count<500) {
+
+if (distance < 1)
+	System.out.println ("this is within the circle");
+	else
+		System.out.println ("this is not within the circle");
+count ++;
+}
 	}
 }
